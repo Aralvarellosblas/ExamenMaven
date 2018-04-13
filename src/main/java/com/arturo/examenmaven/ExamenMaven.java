67b33853fa9de0f9c20d8fab8511fca4dcfd8703
arturo <arturo@ArturoMSI>
2018-04-13 09:37:39 +0200

    Ejercicio frnalizado. Añadido Javadoc

--- src/main/java/com/arturo/examenmaven/ExamenMaven.java
+++ src/main/java/com/arturo/examenmaven/ExamenMaven.java
@@ -1,16 +1,22 @@
 package com.arturo.examenmaven;
 
 /**
+ * Esta clase contiene el metodo principal del programa
  *
  * @author Arturo
  */
-public class ExamenMaven {
+public class ExamenMaven{
 
     /**
+     * Metodo main con el constructor de la clase y las llamadas a los metodos
+     * del programa
+     *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        Metodos m=new Metodos();
+        m.obtenerWeb();
+        m.salidaDatos();
     }
 
 }
